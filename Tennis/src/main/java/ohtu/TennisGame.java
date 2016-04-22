de6f88f6c2cb0fb8d1fd4f00751b399b2b7bf1ba
teks1 <marko.vuorenmaa@cs.helsinki.fi>
2016-04-22 23:32:16 +0300

    Viides viikko valmis

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,11 +1,12 @@
 package ohtu;
 
 public class TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private String score;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,68 +14,67 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName == "player1") {
             m_score1 += 1;
-        else
+        } else {
             m_score2 += 1;
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        this.score = "";
+        if (m_score1 == m_score2) {
+            laskeYkkosPelaajanPisteetJosTasaTilanne();
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            josPisteitaYliNelja();
+        } else {
+            muuTilanne();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        return this.score;
+    }
+
+    private void laskeYkkosPelaajanPisteetJosTasaTilanne() {
+        if (m_score1 < 4) {
+            haePisteString(m_score1);
+            this.score += "-All";
+        } else {
+            this.score = "Deuce";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+    }
+
+    private void josPisteitaYliNelja() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) {
+            score = "Advantage player1";
+        } else if (minusResult == -1) {
+            score = "Advantage player2";
+        } else if (minusResult >= 2) {
+            score = "Win for player1";
+        } else {
+            score = "Win for player2";
         }
-        return score;
+    }
+
+    private void muuTilanne() {
+        haePisteString(m_score1);
+        this.score += "-";
+        haePisteString(m_score2);
+    }
+
+    private void haePisteString(int tempScore) {
+        switch (tempScore) {
+            case 0:
+                this.score += "Love";
+                break;
+            case 1:
+                this.score += "Fifteen";
+                break;
+            case 2:
+                this.score += "Thirty";
+                break;
+            case 3:
+                this.score += "Forty";
+                break;
+        }
     }
 }
